91ae07102b33d4d0b2b3ca98880f8e8800e9c536
Shreha Jalan <shrehajalan1812@gmail.com>
2022-04-23 02:17:35 -0700

    first commit

---
+++ cities_data.js
@@ -0,0 +1,235 @@
+var arrItems = [
+{
+		"city": "City",
+		"admin": "Admin" ,
+		"country": "Country" ,
+		"population_proper": "Population_proper" ,
+		"iso2": "Iso2",
+		"capital": "Capital" ,
+		"lat": "Lat" ,
+		"lng": "Lng",
+		"population": "Population"
+	},
+
+	{
+		"city": "R\u0101mpura",
+		"admin": "R\u0101jasth\u0101n",
+		"country": "India",
+		"population_proper": "2711758",
+		"iso2": "IN",
+		"capital": "",
+		"lat": "26.884682",
+		"lng": "75.789336",
+		"population": "2917000"
+	},
+	
+	{
+		"city": "Bhopal",
+		"admin": "Madhya Pradesh",
+		"country": "India",
+		"population_proper": "1599914",
+		"iso2": "IN",
+		"capital": "admin",
+		"lat": "23.254688",
+		"lng": "77.402892",
+		"population": "1727000"
+	},
+	{
+		"city": "Coimbatore",
+		"admin": "Tamil N\u0101du ",
+		"country": "India",
+		"population_proper": "959823",
+		"iso2": "IN",
+		"capital": "",
+		"lat": "11.005547",
+		"lng": "76.966122",
+		"population": "1696000"
+	},
+	{
+		"city": "Ludhi\u0101na",
+		"admin": "Punjab",
+		"country": "India",
+		"population_proper": "1545368",
+		"iso2": "IN",
+		"capital": "",
+		"lat": "30.912042",
+		"lng": "75.853789",
+		"population": "1649000"
+	},
+	{
+		"city": "\u0100gra",
+		"admin": "Uttar Pradesh",
+		"country": "India",
+		"population_proper": "1430055",
+		"iso2": "IN",
+		"capital": "",
+		"lat": "27.187935",
+		"lng": "78.003944",
+		"population": "1592000"
+	},
+	{
+		"city": "Kaly\u0101n",
+		"admin": "Mah\u0101r\u0101shtra",
+		"country": "India",
+		"population_proper": "1576614",
+		"iso2": "IN",
+		"capital": "",
+		"lat": "19.243703",
+		"lng": "73.135537",
+		"population": "1576614"
+	},
+	{
+		"city": "Vish\u0101khapatnam",
+		"admin": "Andhra Pradesh",
+		"country": "India",
+		"population_proper": "1063178",
+		"iso2": "IN",
+		"capital": "",
+		"lat": "17.704052",
+		"lng": "83.297663",
+		"population": "1529000"
+	},
+	{
+		"city": "Kochi",
+		"admin": "Kerala",
+		"country": "India",
+		"population_proper": "604696",
+		"iso2": "IN",
+		"capital": "",
+		"lat": "9.947743",
+		"lng": "76.253802",
+		"population": "1519000"
+	},
+	{
+		"city": "N\u0101sik",
+		"admin": "Mah\u0101r\u0101shtra",
+		"country": "India",
+		"population_proper": "1289497",
+		"iso2": "IN",
+		"capital": "",
+		"lat": "19.999963",
+		"lng": "73.776887",
+		"population": "1473000"
+	},
+	{
+		"city": "Meerut",
+		"admin": "Uttar Pradesh",
+		"country": "India",
+		"population_proper": "1223184",
+		"iso2": "IN",
+		"capital": "",
+		"lat": "28.980018",
+		"lng": "77.706356",
+		"population": "1398000"
+	},
+	{
+		"city": "Far\u012bd\u0101b\u0101d",
+		"admin": "Hary\u0101na",
+		"country": "India",
+		"population_proper": "1394000",
+		"iso2": "IN",
+		"capital": "",
+		"lat": "28.411236",
+		"lng": "77.313162",
+		"population": "1394000"
+	},
+	{
+		"city": "V\u0101r\u0101nasi",
+		"admin": "Uttar Pradesh",
+		"country": "India",
+		"population_proper": "1164404",
+		"iso2": "IN",
+		"capital": "",
+		"lat": "25.31774",
+		"lng": "83.005811",
+		"population": "1352000"
+	},
+	{
+		"city": "Gh\u0101zi\u0101b\u0101d",
+		"admin": "Uttar Pradesh",
+		"country": "India",
+		"population_proper": "1199191",
+		"iso2": "IN",
+		"capital": "",
+		"lat": "28.665353",
+		"lng": "77.439148",
+		"population": "1341000"
+	},
+	{
+		"city": "\u0100sansol",
+		"admin": "West Bengal",
+		"country": "India",
+		"population_proper": "1328000",
+		"iso2": "IN",
+		"capital": "",
+		"lat": "23.683333",
+		"lng": "86.983333",
+		"population": "1328000"
+	},
+	{
+		"city": "Jamshedpur",
+		"admin": "Jharkhand",
+		"country": "India",
+		"population_proper": "616338",
+		"iso2": "IN",
+		"capital": "",
+		"lat": "22.802776",
+		"lng": "86.185448",
+		"population": "1300000"
+	},
+	{
+		"city": "Madurai",
+		"admin": "Tamil N\u0101du ",
+		"country": "India",
+		"population_proper": "909908",
+		"iso2": "IN",
+		"capital": "",
+		"lat": "9.917347",
+		"lng": "78.119622",
+		"population": "1294000"
+	},
+	{
+		"city": "Jabalpur",
+		"admin": "Madhya Pradesh",
+		"country": "India",
+		"population_proper": "1030168",
+		"iso2": "IN",
+		"capital": "",
+		"lat": "23.174495",
+		"lng": "79.935903",
+		"population": "1285000"
+	},
+	{
+		"city": "R\u0101jkot",
+		"admin": "Gujar\u0101t",
+		"country": "India",
+		"population_proper": "1099882",
+		"iso2": "IN",
+		"capital": "",
+		"lat": "22.291606",
+		"lng": "70.793217",
+		"population": "1260000"
+	},
+	{
+		"city": "Dhanb\u0101d",
+		"admin": "Jharkhand",
+		"country": "India",
+		"population_proper": "219636",
+		"iso2": "IN",
+		"capital": "",
+		"lat": "23.801988",
+		"lng": "86.443244",
+		"population": "1246000"
+	},
+	{
+		"city": "Amritsar",
+		"admin": "Punjab",
+		"country": "India",
+		"population_proper": "1092450",
+		"iso2": "IN",
+		"capital": "",
+		"lat": "31.622337",
+		"lng": "74.875335",
+		"population": "1212000"
+	}
+];
